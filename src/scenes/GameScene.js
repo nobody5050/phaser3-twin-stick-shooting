ae080aa090b3dcac2ccd33d1a6c84f3faf35357d
nobody5050 <32989720+nobody5050@users.noreply.github.com>
2020-10-16 12:24:02 -0500

    more work on movin scenes

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -9,9 +9,59 @@ class GameScene extends Phaser.Scene {
 	}
 
 	create() {
-	   
+		// Create player
+		this.player = this.physics.add.sprite(200, 200, 'player')
+		this.player.setCollideWorldBounds(true)
+		this.player.setOrigin(0.5, 0.72) // Set origin for bullet fire start
 
+		// Create movement joystick
+		this.movementJoyStick = this.plugins.get('rexvirtualjoystickplugin').add(this.scene, {
+			x: 100,
+			y: this.cameras.main.height - 125,
+			radius: 40,
+			forceMin: 0,
+			base: this.add.circle(0, 0, 60, 0x888888).setDepth(100).setAlpha(0.25),
+			thumb: this.add.image(0, 0, 'joystick').setDisplaySize(80, 80).setDepth(100).setAlpha(0.5),
+		}).on('update', () => {}, this)
+		
+		// Create shooting joystick
+		this.shootJoyStick = this.plugins.get('rexvirtualjoystickplugin').add(
+			x: this.cameras.main.width - 100,
+			y: this.cameras.main.height - 125,
+			radius: 20,
+			forceMin: 0,
+			base: this.add.circle(0, 0, 60, 0x888888, 0.5).setDepth(100).setAlpha(0.25),
+			thumb: this.add.image(0, 0, 'joystick').setDisplaySize(80, 80).setDepth(100).setAlpha(0.5),
+	}).on('update', () => {}, this)
+
+		// Move joysticks dynamically based on pointer-down
+		this.input.on('pointerdown', (pointer) => {
+			if (pointer.x <= this.cameras.main.width * 0.4) {
+				this.movementJoyStick.base.setPosition(pointer.x, pointer.y).setAlpha(0.5)
+				this.movementJoyStick.thumb.setPosition(pointer.x, pointer.y).setAlpha(1)
+			}
+			if (pointer.x >= this.cameras.main.width * 0.6) {
+				this.shootJoyStick.base.setPosition(pointer.x, pointer.y).setAlpha(0.5)
+				this.shootJoyStick.thumb.setPosition(pointer.x, pointer.y).setAlpha(1)
+			}
+		})
+
+		// Add transparency to joysticks on pointer-up
+		this.input.on('pointerup', (pointer) => {
+			if (!this.movementJoyStick.force) {
+				this.movementJoyStick.base.setAlpha(0.25)
+				this.movementJoyStick.thumb.setAlpha(0.5)
+			}
+			if (!this.shootJoyStick.force) {
+				this.shootJoyStick.base.setAlpha(0.25)
+				this.shootJoyStick.thumb.setAlpha(0.5)
+			}
+		})
+
+		this.bullets = this.physics.add.group({ classType: Bullet, runChildUpdate: true })
+		this.bulletCooldown = 0
 	}
+}
 
 	update(time, delta) {
 		if (this.bulletCooldown > 0) {
