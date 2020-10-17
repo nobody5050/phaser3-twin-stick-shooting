2ceabffed64a77a8559457e628c78583ce512910
nobody5050 <32989720+nobody5050@users.noreply.github.com>
2020-10-17 00:09:44 -0500

    Please

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -9,6 +9,40 @@ class GameScene extends Phaser.Scene {
 	constructor() {
 		super({key : 'gameScene'});
 	}
+	
+	init() {
+		const MAX_PLAYER_SPEED = 200
+		const BULLET_SPEED = 800
+
+		class Bullet extends Phaser.Physics.Arcade.Sprite {
+			constructor(scene, x, y) {
+				super(scene, x, y, 'bullet')
+
+				this.born = 0
+			}
+
+			fire(shooter) {
+				this.setRotation(shooter.rotation)
+
+				// Offset the bullet to start a bit right of the shooter
+				this.x = shooter.x + (50 * Math.cos(this.rotation))
+				this.y = shooter.y + (50 * Math.sin(this.rotation))
+
+				this.setVelocityX(BULLET_SPEED * Math.cos(Math.PI * this.angle / 180))
+				this.setVelocityY(BULLET_SPEED * Math.sin(Math.PI * this.angle / 180))
+
+				this.born = 0
+			}
+
+			update(time, delta) {
+				this.born += delta
+
+				if (this.born > 1500) {
+					this.destroy()
+				}
+			}
+		}
+	}
 
 	preload() {
 		this.load.image('player', playerImg)
