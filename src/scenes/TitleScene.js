265b37da96d8658d8fe163af76162cfc4b302fd3
nobody5050 <32989720+nobody5050@users.noreply.github.com>
2020-10-18 17:34:26 -0500

    Make scene switching work

--- src/scenes/TitleScene.js
+++ src/scenes/TitleScene.js
@@ -11,12 +11,18 @@ class TitleScene extends Phaser.Scene {
 	}
 
 	create() {
-		 var bg = this.add.sprite(0,0,'background');
-		  bg.setOrigin(0,0);
+		var bg = this.add.sprite(0,0,'background');
+		bg.setOrigin(0,0);
 
-		  var text = this.add.text(100,100, 'Welcome to my game!');
+		var text = this.add.text(100,100, 'Welcome to my game!');
+		text.setInteractive({ useHandCursor: true });
+		text.on('pointerdown', () => this.clickButton());
+		
+		//handle button click
+		clickButton() {
+			this.scene.switch('gameScene');
+		}
 	}
-
 }
 
 export default TitleScene;
