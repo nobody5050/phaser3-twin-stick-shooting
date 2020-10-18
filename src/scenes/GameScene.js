eb9cb7be079d5ba7586a29f5a549f9baf8875f88
nobody5050 <32989720+nobody5050@users.noreply.github.com>
2020-10-17 21:43:05 -0500

    maybe

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -4,36 +4,34 @@ import joystickImg from '../assets/joystick.png'
 import bulletImg from '../assets/bullet.png'
 import rexvirtualjoystickplugin from '../plugins/rexvirtualjoystickplugin.min.js'
 
+const MAX_PLAYER_SPEED = 200
+const BULLET_SPEED = 800
+
+class Bullet extends Phaser.Physics.Arcade.Sprite {
+	constructor(scene, x, y) {
+		super(scene, x, y, 'bullet')
+		
+		this.born = 0
+	}
+
+	fire(shooter) {
+		this.setRotation(shooter.rotation)
+
+		// Offset the bullet to start a bit right of the shooter
+		this.x = shooter.x + (50 * Math.cos(this.rotation))
+		this.y = shooter.y + (50 * Math.sin(this.rotation))
+
+		this.setVelocityX(BULLET_SPEED * Math.cos(Math.PI * this.angle / 180))
+		this.setVelocityY(BULLET_SPEED * Math.sin(Math.PI * this.angle / 180))
+
+		this.born = 0
+	}
+}
+
 class GameScene extends Phaser.Scene {
 
 	constructor() {
 		super({key : 'gameScene'});
-	}
-	
-	init() {
-		const MAX_PLAYER_SPEED = 200
-		const BULLET_SPEED = 800
-
-		class Bullet extends Phaser.Physics.Arcade.Sprite {
-			constructor(scene, x, y) {
-				super(scene, x, y, 'bullet')
-
-				this.born = 0
-			}
-
-			fire(shooter) {
-				this.setRotation(shooter.rotation)
-
-				// Offset the bullet to start a bit right of the shooter
-				this.x = shooter.x + (50 * Math.cos(this.rotation))
-				this.y = shooter.y + (50 * Math.sin(this.rotation))
-
-				this.setVelocityX(BULLET_SPEED * Math.cos(Math.PI * this.angle / 180))
-				this.setVelocityY(BULLET_SPEED * Math.sin(Math.PI * this.angle / 180))
-
-				this.born = 0
-			}
-		}
 	}
 	
 	preload() {
