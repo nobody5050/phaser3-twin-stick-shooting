ee4951f7c2601c157c23bc467a1b40b8f05b4c4c
nobody5050 <32989720+nobody5050@users.noreply.github.com>
2020-10-17 21:58:09 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,8 +10,8 @@ const config = {
   type: Phaser.AUTO,
   parent: 'phaser-example',
   backgroundColor: '#56ac68',
-  width: '100%',
-  height: '100%',
+  width: 1920,
+  height: 1080,
   physics: {
     default: 'arcade',
   },
