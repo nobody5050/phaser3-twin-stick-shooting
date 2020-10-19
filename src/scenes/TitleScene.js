d7a4d73e7957b927dc207353fdc1c668b31fa5bc
nobody5050 <32989720+nobody5050@users.noreply.github.com>
2020-10-18 19:53:59 -0500

    Update TitleScene.js

--- src/scenes/TitleScene.js
+++ src/scenes/TitleScene.js
@@ -15,11 +15,9 @@ class TitleScene extends Phaser.Scene {
 		var bg = this.add.sprite(0,0,'background');
 		bg.setOrigin(0,0);
 
-		var text = this.add.text(100,100, 'Welcome to my game!');
+		var text = this.add.text(100,100, 'enter test lobby');
 		text.setInteractive({ useHandCursor: true });
-		text.on('pointerdown', () => this.clickButton());
-		
-		clickButton(){this.scene.switch('gameScene');}
+		text.on('pointerdown', () => this.scene.switch('gameScene'););
 	}
 }
 
