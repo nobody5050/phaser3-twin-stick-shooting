7734a6f279662a840c2dab39262d6ba8f26cd708
nobody5050 <32989720+nobody5050@users.noreply.github.com>
2020-11-06 16:30:54 -0600

    Update client.js

--- src/components/client.js
+++ src/components/client.js
@@ -1,2 +1,14 @@
 //todo
 let colyseusClient = new Colyseus.Client("ws://134.209.68.198:2567");
+connect.setInteractive({useHandCursor: true});
+		connect.on('pointerdown', () => {
+			try {
+				const room = client.joinOrCreate("battle", {/* options */});
+				console.log("joined successfully", room);
+				serverJoin = this.add.text(window.innerWidth*0.05,window.innerHeight*0.05, 'joined server sucessfully');
+
+			} catch (e) {
+				console.error("join error", e);
+				serverJoin = this.add.text(window.innerWidth*0.05,window.innerHeight*0.05, 'join error');
+			}
+		})
