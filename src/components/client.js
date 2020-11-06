321d241ae5e39510960163611d4a74f732dfd4a5
nobody5050 <32989720+nobody5050@users.noreply.github.com>
2020-11-06 17:16:04 -0600

    Update client.js

--- src/components/client.js
+++ src/components/client.js
@@ -1,6 +1,6 @@
 import Phaser from 'phaser';
 
-var connect = phaser.add.text(200,200, 'conncect');
+var connect = Phaser.add.text(200,200, 'conncect');
 let client = new Colyseus.Client("ws://134.209.68.198:2567");
 connect.setInteractive({useHandCursor: true});
 function clientJoin() { 
