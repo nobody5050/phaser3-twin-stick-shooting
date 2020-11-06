e3dcfd4594ec0400445d4f2e9e369e26957b7371
nobody5050 <32989720+nobody5050@users.noreply.github.com>
2020-11-06 16:37:08 -0600

    Update client.js

--- src/components/client.js
+++ src/components/client.js
@@ -4,14 +4,17 @@ import Phaser from 'phaser'
 var connect = phaser.add.text(200,200, 'conncect');
 let colyseusClient = new Colyseus.Client("ws://134.209.68.198:2567");
 connect.setInteractive({useHandCursor: true});
-connect.on('pointerdown', () => {
-	try {
-		const room = client.joinOrCreate("battle", {/* options */});
-		console.log("joined successfully", room);
-		serverJoin = this.add.text(window.innerWidth*0.05,window.innerHeight*0.05, 'joined server sucessfully');
+function clientJoin() { 
+	connect.on('pointerdown', () => {
+		try {
+			const room = client.joinOrCreate("battle", {/* options */});
+			console.log("joined successfully", room);
+			serverJoin = this.add.text(window.innerWidth*0.05,window.innerHeight*0.05, 'joined server sucessfully');
 
-	} catch (e) {
-		console.error("join error", e);
-		serverJoin = this.add.text(window.innerWidth*0.05,window.innerHeight*0.05, 'join error');
-	}
-})
+		} catch (e) {
+			console.error("join error", e);
+			serverJoin = this.add.text(window.innerWidth*0.05,window.innerHeight*0.05, 'join error');
+		}
+	})
+}
+export { connect, client, clientJoin };
