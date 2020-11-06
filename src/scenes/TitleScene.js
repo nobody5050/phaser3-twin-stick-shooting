2b0835a7d7a778507e11d4e7fdd0f7fa03bd4a3c
nobody5050 <32989720+nobody5050@users.noreply.github.com>
2020-11-06 12:57:11 -0600

    Fix deploy time

--- src/scenes/TitleScene.js
+++ src/scenes/TitleScene.js
@@ -2,8 +2,8 @@ import Phaser from 'phaser';
 import GameScene from './GameScene.js';
 import discord from '../assets/socialMediaIcons/discordJoinLogo.svg';
 
-import Colyseus from "colyseus.js";
-let colyseusClient = new Colyseus.Client("ws://134.209.68.198:2567");
+//import Colyseus from "colyseus.js";
+//let colyseusClient = new Colyseus.Client("ws://134.209.68.198:2567");
 
 class TitleScene extends Phaser.Scene {
 
