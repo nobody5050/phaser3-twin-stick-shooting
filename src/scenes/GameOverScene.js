5fd548ddbdd97f3d80ac93e10d55c15c0f297078
StealthHydra179 <aidenm888@gmail.com>
2020-11-11 18:35:32 -0500

    gui updates

---
+++ src/scenes/GameOverScene.js
@@ -0,0 +1,27 @@
+import Phaser from 'phaser';
+import GameScene from './GameScene.js';
+import {client, clientJoin , room } from "../components/client.js";
+
+class GameOverScene extends Phaser.Scene {
+
+    constructor() {
+        super({key: 'gameOverScene'});
+    }
+
+    preload() {
+
+    }
+
+    create() {
+        var rejoin = this.add.text(100,100,"rejoin")
+            .setInteractive({useHandCursor: true})
+        var serverJoin
+        serverJoin = clientJoin(serverJoin, this, text, "battle")
+        rejoin.on('pointerdown', () => {
+            this.scene.switch('gameScene')
+        });
+    }
+
+}
+
+export default TitleScene;
--- src/scenes/TitleScene.js
+++ src/scenes/TitleScene.js
@@ -34,17 +34,39 @@ class TitleScene extends Phaser.Scene {
 		text.on('pointerdown', () => {
 			this.scene.switch('gameScene')
 		});
+		var simulateGameEnd = this.add.text(100, 150, 'simulate game end')
+		simulateGameEnd.on('pointerdown', () => {
+			this.scene.switch('gameOverScene')
+		})
 
 		// var connect = this.add.text(200,200, 'conncect');
 		// connect.setInteractive({useHandCursor: true});
 		// serverJoin = clientJoin(serverJoin, this, connect, "battle")
 
 		// this.textures.addImage("discordJoinImage", 'src/assets/socialMediaIcons/discordJoinLogo.png')
-		var discordIcon = this.add.image(window.innerWidth*0.05, window.innerHeight*0.95, 'discordJoinImage').setDisplaySize(20, 30)
 
-		var dokmiBoxOutline = this.add.rectangle(window.innerWidth*0.25, 300, window.innerWidth*0.5, 200, "white")
-		var dokmiBoxOutline = this.add.rectangle(window.innerWidth*0.25+5, 305, window.innerWidth*0.5-10, 190, "#56ac68")
+		/*
+		Window needs to be greater than:
+			currently 700px tall
+			and 1000 px wide, might change in the future
+		 */
+		let valid = true;
+		if (window.innerHeight < 700) {
+			console.log("page not tall enough")
+			valid = false
+		}
+		if (window.innerWidth < 1000) {
+			console.log("page not wide enough")
+			valid = false
+		}
+		if (valid) {
+			var discordIcon = this.add.image(window.innerWidth * 0.05, window.innerHeight * 0.95, 'discordJoinImage').setDisplaySize(20, 30)
 
+			var dokmiBoxOutline = this.add.rectangle(window.innerWidth * 0.25, 300, window.innerWidth * 0.5, 200, "white")
+			var dokmiBoxOutline = this.add.rectangle(window.innerWidth * 0.25 + 5, 295, window.innerWidth * 0.5 - 10, 190, "#56ac68")
+		} else {
+			var invalid = this.add.text(window.innerWidth*0.1, window.innerHeight*0.5, "screen size not valid")
+		}
 	}
 }
 
