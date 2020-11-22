9a54d3fc884317fb856759c3095e7fa1abd27228
StealthHydra179 <aidenm888@gmail.com>
2020-11-22 12:55:39 -0500

    Update TitleScene.js

--- src/scenes/TitleScene.js
+++ src/scenes/TitleScene.js
@@ -64,9 +64,9 @@ class TitleScene extends Phaser.Scene {
 			})
 
 
-			var dokmiBoxOutline = this.add.rectangle(window.innerWidth * 0.50, window.innerHeight*0.25, window.innerWidth * 0.50, 200, 0xFFFFFF)
-			var dokmiBoxFill = this.add.rectangle(window.innerWidth * 0.50, window.innerHeight*0.25, window.innerWidth * 0.50 - 10, 190, 0x56ac68)
-			var dokmiText = this.add.text(window.innerWidth*0.25, window.innerHeight*0.25-80, "DOKMI.IO",{fontsize: window.innerWidth*0.5})
+			var dokmiBoxOutline = this.add.rectangle(window.innerWidth * 0.50, window.innerHeight*0.25, 500, 200, 0xFFFFFF)
+			var dokmiBoxFill = this.add.rectangle(window.innerWidth * 0.50, window.innerHeight*0.25, 490, 190, 0x56ac68)
+			var dokmiText = this.add.text(window.innerWidth*0.25, window.innerHeight*0.25-80, "DOKMI.IO",{fontSize: window.innerWidth*0.5})
 		} else {
 			var invalid = this.add.text(window.innerWidth*0.1, window.innerHeight*0.5, "screen size not valid")
 		}
