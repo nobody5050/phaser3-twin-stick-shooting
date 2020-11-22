980913442e9489a123504ac733d7c4b55977ce3c
StealthHydra179 <aidenm888@gmail.com>
2020-11-22 12:46:45 -0500

    Update LoginScene.js

--- src/scenes/LoginScene.js
+++ src/scenes/LoginScene.js
@@ -1,6 +1,6 @@
 import Phaser from 'phaser';
 import TitleScene from "./TitleScene.js";
-import SignUpScene from "./SignUpScene,js";
+import SignUpScene from "./SignUpScene.js";
 
 class LoginScene extends Phaser.Scene  {
 
