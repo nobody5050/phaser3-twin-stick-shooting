02972b52b53c7d684bb7e9544f3101c4d928432c
StealthHydra179 <aidenm888@gmail.com>
2020-12-17 17:40:22 -0500

    Update GunsHandler.js

--- src/components/guns/GunsHandler.js
+++ src/components/guns/GunsHandler.js
@@ -1,4 +1,4 @@
 //this file will impport all the different gun files and export them to GameScene from one file for convinence
-import { stats as DevGun} from 'Dev.js';
+import { stats as DevGun} from '/Dev.js';
 
 export { DevGun } //looks dum rn but you can add more exports into this file for easy import anywhere needed
