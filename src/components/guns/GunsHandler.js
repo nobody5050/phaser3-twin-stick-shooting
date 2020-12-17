4b7a7f25e4764a98423851f2cfa06d3882bc3435
StealthHydra179 <aidenm888@gmail.com>
2020-12-17 18:20:06 -0500

    Update GunsHandler.js

--- src/components/guns/GunsHandler.js
+++ src/components/guns/GunsHandler.js
@@ -1,4 +1,4 @@
-//this file will import all the different gun files and export them to GameScene from one file for convinence
+//this file will import all the different gun files and export them to GameScene from one file for convenience
 import { stats as DevGun } from '../guns/Dev.js';
 import { stats as Hi_Power } from '../guns/Hi-Power.js';
 
