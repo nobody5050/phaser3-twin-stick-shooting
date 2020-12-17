493f23c5dadfdff10bd01d7ced7dde5ee51243e9
StealthHydra179 <aidenm888@gmail.com>
2020-12-17 18:13:37 -0500

    Update Hi-Power.js

---
+++ src/components/guns/Hi-Power.js
@@ -0,0 +1,17 @@
+let stats = {
+    name: 'High-Power',
+    release: 'launch',
+    tier: 3,
+    DMG: 21,
+    AP: 1,
+    projectileCount: 1,
+    delay: 0.2,
+    spread: 3,
+    range: 25,
+    speed: 50,
+    mag: 12,
+    reload: 1.5,
+    spawnAmmo: 36
+}
+
+export { stats };
