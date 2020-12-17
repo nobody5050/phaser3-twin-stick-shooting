2a6c656a2786f68b38eca21fb5afc49a9c32cfd3
StealthHydra179 <aidenm888@gmail.com>
2020-12-17 18:05:41 -0500

    Update Dev.js

--- src/components/guns/Dev.js
+++ src/components/guns/Dev.js
@@ -1,7 +1,13 @@
 let stats = {
-    //are these realistic?
-    speed: 800,
-    range: 100
+    DMG: 20,
+    AP: 2,
+    delay: 0.2,
+    spread: 2,
+    range: 50,
+    speed: 50,
+    mag: 25,
+    reload: 1,
+    spawnAmmo: 50
 }
 
 export { stats };
