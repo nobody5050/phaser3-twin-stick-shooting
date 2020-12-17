c07fba2005359d3ccd002920747a1c4faf52aaba
StealthHydra179 <aidenm888@gmail.com>
2020-12-17 17:45:31 -0500

    fix patch

--- Dev.js
+++
@@ -1,7 +0,0 @@
-let stats = {
-    //are these realistic?
-    speed: 800,
-    range: 100
-}
-
-export { stats };
--- src/components/guns/GunsHandler.js
+++ src/components/guns/GunsHandler.js
@@ -1,4 +1,4 @@
-//this file will impport all the different gun files and export them to GameScene from one file for convinence
-import { stats as DevGun} from '/Dev.js';
+//this file will import all the different gun files and export them to GameScene from one file for convinence
+import { stats as DevGun } from 'src/components/guns/Dev.js';
 
 export { DevGun } //looks dum rn but you can add more exports into this file for easy import anywhere needed
