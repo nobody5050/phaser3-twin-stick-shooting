e7e2cb5c7b1b4eb57c4a65bbd46ce38b6110685f
nobody5050 <32989720+nobody5050@users.noreply.github.com>
2020-12-16 21:30:29 -0600

    Create GunsHandler.js

---
+++ src/components/guns/GunsHandler.js
@@ -0,0 +1,4 @@
+//this file will impport all the different gun files and export them to GameScene from one file for convinence
+import DevGun from 'dev.js';
+
+export { DevGun } //looks dum rn but you can add more exports into this file for easy import anywhere needed
