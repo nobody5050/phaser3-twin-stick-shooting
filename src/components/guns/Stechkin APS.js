3085e89bec2990588150c0d7dcf5c639539d14cb
StealthHydra179 <aidenm888@gmail.com>
2020-12-17 18:23:31 -0500

    Update Stechkin APS.js

---
+++ src/components/guns/Stechkin APS.js
@@ -0,0 +1,18 @@
+let stats = {
+    name: 'Stechkin APS',
+    release: 'launch',
+    category: 'Handgun',
+    tier: 2,
+    DMG: 13,
+    AP: 1,
+    projectileCount: 1,
+    delay: 0.08,
+    spread: 7,
+    range: 24,
+    speed: 45,
+    mag: 20,
+    reload: 1.8,
+    spawnAmmo: 60
+}
+
+export { stats };
