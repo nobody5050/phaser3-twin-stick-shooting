95acb203dbdca9f7a6671d45a3624e4f4e13be79
StealthHydra179 <aidenm888@gmail.com>
2021-01-22 08:52:09 -0500

    Update TitleScene.js

--- src/scenes/TitleScene.js
+++ src/scenes/TitleScene.js
@@ -15,7 +15,7 @@ class TitleScene extends Phaser.Scene {
     preload() {
         this.load.svg('discordJoinImage', discord, {scale: 0.1})
         this.load.svg('patreonJoinImage', patreon, {scale: 0.5})
-
+        
     }
 
     create() {
@@ -42,34 +42,34 @@ class TitleScene extends Phaser.Scene {
             currently 700px tall
             and 1000 px wide, might change in the future
          */
-        let valid = true;
-        if (window.innerHeight < 700) {
-            console.log("page not tall enough")
-            valid = false
-        }
-        if (window.innerWidth < 700) {
-            console.log("page not wide enough")
-            valid = false
-        }
-        if (valid) {
-            var discordIcon = this.add.image(30, window.innerHeight - 30, 'discordJoinImage')
-            discordIcon.setInteractive({useHandCursor: true})
-            discordIcon.on('pointerdown', () => {
-                let discordWebsite = window.open('https://discord.gg/mEF8nTF2DX')
-            })
-            var patreonIcon = this.add.image(60, window.innerHeight - 30, 'patreonJoinImage')
-            patreonIcon.setInteractive({useHandCursor: true})
-            patreonIcon.on('pointerdown', () => {
-                let patreonWebsite = window.open('https://www.patreon.com/dokmiOfficial')
-            })
+        // let valid = true;
+        // if (window.innerHeight < 700) {
+        //     console.log("page not tall enough")
+        //     valid = false
+        // }
+        // if (window.innerWidth < 700) {
+        //     console.log("page not wide enough")
+        //     valid = false
+        // }
+        // if (valid) {
+        var discordIcon = this.add.image(30, window.innerHeight - 30, 'discordJoinImage')
+        discordIcon.setInteractive({useHandCursor: true})
+        discordIcon.on('pointerdown', () => {
+            let discordWebsite = window.open('https://discord.gg/mEF8nTF2DX')
+        })
+        var patreonIcon = this.add.image(60, window.innerHeight - 30, 'patreonJoinImage')
+        patreonIcon.setInteractive({useHandCursor: true})
+        patreonIcon.on('pointerdown', () => {
+            let patreonWebsite = window.open('https://www.patreon.com/dokmiOfficial')
+        })
 
 
-            // var dokmiBoxOutline = this.add.rectangle(window.innerWidth * 0.50, window.innerHeight*0.25, 650, 200, 0xFFFFFF)
-            // var dokmiBoxFill = this.add.rectangle(window.innerWidth * 0.50, window.innerHeight*0.25, 640, 190, 0x56ac68)
-            // var dokmiText = this.add.text(window.innerWidth*0.25 + 100, window.innerHeight*0.25-60, "DOKMI.IO",{fontSize: 125})
-        } else {
-            var invalid = this.add.text(window.innerWidth * 0.1, window.innerHeight * 0.5, "screen size not valid")
-        }
+        // var dokmiBoxOutline = this.add.rectangle(window.innerWidth * 0.50, window.innerHeight*0.25, 650, 200, 0xFFFFFF)
+        // var dokmiBoxFill = this.add.rectangle(window.innerWidth * 0.50, window.innerHeight*0.25, 640, 190, 0x56ac68)
+        // var dokmiText = this.add.text(window.innerWidth*0.25 + 100, window.innerHeight*0.25-60, "DOKMI.IO",{fontSize: 125})
+        // } else {
+        //     var invalid = this.add.text(window.innerWidth * 0.1, window.innerHeight * 0.5, "screen size not valid")
+        // }
     }
 }
 
