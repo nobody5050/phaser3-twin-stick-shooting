2820e256100ee83e3829f5ea20a53f5c40c3a265
nobody5050 <32989720+nobody5050@users.noreply.github.com>
2021-01-22 19:20:20 -0600

    Update GameScene.js

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -24,8 +24,8 @@ class Bullet extends Phaser.Physics.Arcade.Sprite {
         this.setRotation(shooter.rotation)
 
         // Offset the bullet to start a bit right of the shooter
-        this.x = shooter.x + (50 * Math.cos(this.rotation))
-        this.y = shooter.y + (50 * Math.sin(this.rotation))
+        this.x = shooter.x + (100 * Math.cos(this.rotation))
+        this.y = shooter.y + (100 * Math.sin(this.rotation))
 
 
         this.setVelocityX(BULLET_SPEED * Math.cos(Math.PI * this.angle / 180))
