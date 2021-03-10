dfe5d965ec9a10cb9f170687a65708744ca84fc7
nobody5050 <32989720+nobody5050@users.noreply.github.com>
2021-03-09 18:14:10 -0600

    Delete LoginScene.js

--- src/scenes/LoginScene.js
+++
@@ -1,9 +0,0 @@
-import Phaser from 'phaser';
-import TitleScene from "./TitleScene.js";
-import SignUpScene from "./SignUpScene.js";
-
-class LoginScene extends Phaser.Scene  {
-
-}
-
-export default LoginScene;
