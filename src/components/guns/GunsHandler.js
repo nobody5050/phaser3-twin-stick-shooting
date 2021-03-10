1e9c01b50c3b27aacc3e7669a26fe59cb8329ee9
nobody5050 <32989720+nobody5050@users.noreply.github.com>
2021-03-09 18:13:13 -0600

    Delete GunsHandler.js

--- src/components/guns/GunsHandler.js
+++
@@ -1,6 +0,0 @@
-//this file will import all the different gun files and export them to GameScene from one file for convenience
-import { stats as Dev } from '../guns/Dev.js';
-import { stats as Hi_Power } from '../guns/Hi-Power.js';
-import { stats as Stechkin_APS } from '../guns/Stechkin APS.js';
-
-export { Dev, Hi_Power, Stechkin_APS }
